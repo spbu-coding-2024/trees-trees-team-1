73e11f84691bac6676fe2502aca10eef02fc03ef
Darya Nechaeva <necaevadara435@gmail.com>
2025-03-17 23:10:28 +0300

    add helper functions for height

--- src/main/kotlin/AVLNode.kt
+++ src/main/kotlin/AVLNode.kt
@@ -1,4 +1,19 @@
 class AVLNode<K: Comparator<K>, T>(override var key: K, override var value: T) : Node<K, T, AVLNode<K, T>>() {
-    private var height: Int=0
-    private fun checkHeight(): Int {}
+	private var height: Int=0
+
+	// Может работать с null
+	private fun checkHeight(node: AVLNode<K, T>?): Int {
+		return node?.height ?: 0
+	}
+
+	// Только с не null
+	private fun calculateBFactor(node: AVLNode<K, T>): Int {
+		return height(node.right) - height(node.left)
+	}
+
+	private fun fixHeight(node: AVLNode<K, T>) {
+		val hl = height(node.left)
+		val hr = height(node.right)
+		node.height = maxOf(hl, hr) + 1
+	}
 }
