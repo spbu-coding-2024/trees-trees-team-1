60b9dfd75d05135c68f030062c328ef0ce767cc3
Darya Nechaeva <necaevadara435@gmail.com>
2025-03-18 21:31:01 +0300

    add left and right rotation function

--- src/main/kotlin/AVLTree.kt
+++ src/main/kotlin/AVLTree.kt
@@ -1,6 +1,35 @@
 class AVLTree<K:Comparator<K>, T>(): BinaryTree<K,T, AVLNode<K, T>> {
-    private var root: AVLNode<K, T>?=null
+	private var root: AVLNode<K, T>?=null
 
-    private fun leftRotation(node: AVLNode<K, T>) {}
-    private fun rightRotation(node: AVLNode<K, T>) {}
+	private fun swapNodes(nodeA: AVLNode<K, T>, nodeB: AVLNode<K, T>) {
+		aKey: K = nodeA.key
+		nodeA.key = nodeB.key
+		nodeB.key = aKey
+		aValue: T = nodeA.value
+		nodeA.value = nodeB.value
+		nodeB.value = aValue
+	}
+
+	private fun leftRotation(node: AVLNode<K, T>) {
+		swapNodes(node, node.right)
+		buffer: Node = node.left
+		node.left = node.right
+		node.right = node.left.right
+		node.right.left = node.right.right
+		node.left.right = node.left.left
+		node.left.left = buffer
+		fixHeight(node.left)
+		fixHeight(node)
+	}
+
+	private fun rightRotation(node: AVLNode<K, T>) {
+		swapNodes(node, node.left)
+		buffer: Node = node.right
+		node.right = node.left
+		node.left = node.right.left
+		node.right.left = node.right.right
+		node.right.right = buffer
+		fixHeight(node.right)
+		fixHeight(node)
+	}
 }
