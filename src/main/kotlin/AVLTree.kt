93ee39d535a57118c529f2ac115a42d9cecb4cdd
Darya Nechaeva <necaevadara435@gmail.com>
2025-03-22 00:08:48 +0300

    add functions for node removal

--- src/main/kotlin/AVLTree.kt
+++ src/main/kotlin/AVLTree.kt
@@ -10,7 +10,7 @@ abstract class AVLTree<K:Comparable<K>, T>(): BinaryTree<K,T, AVLNode<K, T>>() {
 	}
 
 	private fun leftRotation(node: AVLNode<K, T>): AVLNode<K, T>? {
-		val rightChild = node.right ?: return null		// Если правый потомок null, возвращаем null
+		val rightChild = node.right ?: return null	// Если правый потомок null, возвращаем null
 		swapNodes(node, rightChild)
 		val buffer: Node = node.left
 		node.left = rightChild
@@ -55,7 +55,7 @@ abstract class AVLTree<K:Comparable<K>, T>(): BinaryTree<K,T, AVLNode<K, T>>() {
 		}
 	}
 
-	override fun insert(root: AVLNode<K, T>?, key: K, value: T): AVLNode<K, T> {
+	override fun insert(root: AVLNode<K, T>?, key: K, value: T): AVLNode<K, T>? {
 		if (root == null) return AVLNode(key, value)
 		if (key < root.key) {
 			root.left = insert(root.left, key, value)
@@ -65,6 +65,39 @@ abstract class AVLTree<K:Comparable<K>, T>(): BinaryTree<K,T, AVLNode<K, T>>() {
 		}
 		else {
 			root.value = value
+		}
+		return balance(root)
+	}
+
+	private fun findMin(node: AVLNode<K, T>): AVLNode<K, T> {
+		var current = node
+		while (current.left != null) {
+			current = current.left ?: break
+		}
+		return current
+	}
+
+	private fun removeMin(node: AVLNode<K, T>): AVLNode<K,T>? {
+		if (node.left == null) return node.right
+		node.left = removeMin(node.left ?: return node.right)
+		return balance(node)
+	}
+
+	override fun delete(root: AVLNode<K, T>?, key: K): AVLNode<K, T>? {
+		if (root == null) return null
+		if (key < root.key) {
+			root.left = delete(root.left, key)
+		}
+		else if (key > root.key) {
+			root.right = delete(root.right, key)
+		} else {
+				val left = root.left
+				val right = root.right
+				if (right == null) return left
+				val minNode = findMin(right)
+				minNode.right = removeMin(right)
+				minNode.left = left
+				return balance(minNode)
 		}
 		return balance(root)
 	}
