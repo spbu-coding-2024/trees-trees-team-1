fa083e97b7b4421d2afb316020296a5298dc76ed
Mikhail <mihailsvirukov@mail.ru>
2025-03-23 19:22:44 +0300

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,5 +17,44 @@ tasks.test {
     useJUnitPlatform()
 }
 kotlin {
+<<<<<<< HEAD
     jvmToolchain(22)
+=======
+    jvmToolchain(23)
+}
+
+
+tasks.register<Test>("RBBasic") {
+    useJUnitPlatform { filter { includeTags("basic & BRTree") } }
+}
+
+tasks.register<Test>("RBInsert") {
+    dependsOn("RBBasic")
+    useJUnitPlatform { filter{ includeTags("insert & BRTree")} }
+}
+
+tasks.register<Test>("RBDelete") {
+    dependsOn("RBInsert")
+    useJUnitPlatform { filter { includeTags("delete & BRTree") } }
+}
+
+tasks.register<Test>("testRBtree") {
+    dependsOn("RBDelete")
+}
+
+tasks.named<Test>("test") {
+    dependsOn("testRBtree")
+    finalizedBy(tasks.findByPath("jacocoTestReport"))
+}
+
+
+
+tasks.named<JacocoReport>("jacocoTestReport") {
+    dependsOn(tasks.test)
+    reports {
+        csv.required = false
+        xml.required = false
+        html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+    }
+>>>>>>> 3b04d04 (fix)
 }
