9db548b1f8e581ee4bdfb043c510bda322fb3c7b
Darya Nechaeva <necaevadara435@gmail.com>
2025-03-25 22:59:56 +0300

    fix rotations and balance functions for height functions

--- src/main/kotlin/AVLTree.kt
+++ src/main/kotlin/AVLTree.kt
@@ -10,51 +10,50 @@ abstract class AVLTree<K:Comparable<K>, T>(): BinaryTree<K,T, AVLNode<K, T>>() {
 	}
 
 
-	private fun leftRotation(node: AVLNode<K, T>): AVLNode<K, T>? {
-		val rightChild = node.right ?: return null	// Если правый потомок null, возвращаем null
+	private fun leftRotation(node: AVLNode<K, T>): AVLNode<K, T> {
+		val rightChild = node.right ?: return node
 		swapNodes(node, rightChild)
 		val buffer = node.left
 		node.left = rightChild
 		node.right = rightChild.right
 		rightChild.right = rightChild.left
 		rightChild.left = buffer
-		node.fixHeight(node)
-		rightChild.fixHeight(rightChild)
-		return rightChild
+		rightChild.fixHeight()
+		node.fixHeight()
+		return node
 	}
 
 
-	private fun rightRotation(node: AVLNode<K, T>): AVLNode<K, T>? {
-		val leftChild = node.left ?: return null //Если левый потомок null, возвращаем null
+	private fun rightRotation(node: AVLNode<K, T>): AVLNode<K, T> {
+		val leftChild = node.left ?: return node
 		swapNodes(node, leftChild)
 		val buffer = node.right
 		node.right = leftChild
 		node.left = leftChild.left
 		leftChild.left = leftChild.right
 		leftChild.right = buffer
-		node.fixHeight(node)
-		leftChild.fixHeight(leftChild)
-		return leftChild
+		leftChild.fixHeight()
+		node.fixHeight()
+		return node
 	}
 
 
-	private fun balance(node: AVLNode<K, T>?): AVLNode<K, T>? {
-		if (node == null) return null
-		node.fixHeight(node)
-		val balanceFactor: Int = node.calculateBalanceFactor(node)
+	private fun balance(node: AVLNode<K, T>): AVLNode<K, T> {
+		node.fixHeight()
+		val balanceFactor = node.calculateBalanceFactor()
 		if (balanceFactor == -2) {		//Если левое поддерево выше
 			val leftChild = node.left
-			if (leftChild != null && leftChild.calculateBalanceFactor(leftChild) == 1) {
+			if (leftChild != null && leftChild.calculateBalanceFactor() == 1) {
 				node.left = leftRotation(leftChild)
 			}
-			return rightRotation(node) ?: node
+			return rightRotation(node)
 		}
 		else if (balanceFactor == 2) {		//Если правое поддерево выше
 			val rightChild = node.right
-			if (rightChild != null && rightChild.calculateBalanceFactor(rightChild) == -1) {
+			if (rightChild != null && rightChild.calculateBalanceFactor() == -1) {
 				node.right = rightRotation(rightChild)
 			}
-			return leftRotation(node) ?: node
+			return leftRotation(node)
 		}
 		else {
 			return node		//Если балансировка не требуется
