41cb46d6f92b3151c9e12737456a4bfa52882800
Darya Nechaeva <necaevadara435@gmail.com>
2025-03-27 14:17:40 +0300

    add tests for find function

--- src/test/kotlin/AVLTree/Basic.kt
+++ src/test/kotlin/AVLTree/Basic.kt
@@ -65,4 +65,53 @@ class basic {
 		val expected = "3 null null "
 		assertEquals(expected, tree.printNodes())
 	}
+
+	@Tag("find")
+	@Test
+	@DisplayName("Checking find existing elements")
+	fun findExisting() {
+		tree.insert(10, 1)
+		tree.insert(7, 1)
+		tree.insert(3, 1)
+		assertEquals(true, tree.find(10))
+		assertEquals(true, tree.find(7))
+		assertEquals(true, tree.find(3))
+	}
+
+	@Tag("find")
+	@Test
+	@DisplayName("Checking find non-existing elements")
+	fun findNonExisting() {
+		tree.insert(10, 1)
+		tree.insert(7, 1)
+		tree.insert(3, 1)
+		assertEquals(false, tree.find(11))
+		assertEquals(false, tree.find(6))
+		assertEquals(false, tree.find(1))
+	}
+
+	@Tag("find")
+	@Test
+	@DisplayName("checking find function for existing and non-existing elements")
+	fun checkingFind() {
+		val values = listOf(7, 10, 11, 3)
+		for (i in values) {
+			tree.insert(i, 1)
+		}
+		val actual = mutableListOf<Boolean>()
+		actual.addLast(tree.find(1))
+		for (i in values) {
+			actual.addLast(tree.find(i))
+		}
+		actual.addLast(tree.find(8))
+		val expected = listOf(false, true, true, true, true, false)
+		assertEquals(expected, actual)
+	}
+
+	@Tag("find")
+	@Test
+	@DisplayName("checking find for empty tree")
+	fun findForEmptyTree() {
+		assertEquals(false, tree.find(10))
+	}
 }
